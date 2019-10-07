120091f4f8654d299e8946cfd1ea500b6ad7467f
Sofiya Karpa <sofiya.karpa@apostera.com>
2019-10-07 17:31:16 +0300

    Modification of excel file

--- jiraautomation/operations/generate_wbs_excel.py
+++ jiraautomation/operations/generate_wbs_excel.py
@@ -3,6 +3,9 @@ from openpyxl import load_workbook
 
 from .generate_wbs_json import generate_wbs_json
 import pandas as pd
+import numpy as np
+import yaml
+
 
 class generate_wbs_excel(basic_operation):
 
@@ -12,7 +15,8 @@ class generate_wbs_excel(basic_operation):
 
     @staticmethod
     def init_arguments(operation_group):
-        pass
+        operation_group.add_argument('-gwbss', '--generatewbs_sprints2fcs', required=False,
+                                     help='Path to YAML file containing fcs and its corresponding sprints')
 
     @staticmethod
     def parse_arguments(args):
@@ -32,16 +36,13 @@ class generate_wbs_excel(basic_operation):
                 json_data = op.execute(container, args)
                 df = pd.read_json(json_data)
 
-                with pd.ExcelWriter(args.output, engine='openpyxl') as writer:
-                    writer.book = load_workbook(args.output)
-                    writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
-                    sheet = writer.book.sheetnames[0]
-                    for row in writer.book[sheet]:
-                        for cell in row:
-                            cell.value = None
+                with open(args.generatewbs_sprints2fcs) as f:
+                    fcs_sprints = yaml.load(f, Loader=yaml.Loader)
 
-                    full_df = calculated_cols(df)
-                    full_df.to_excel(writer, sheet_name=sheet, index=False)
+                full_df = calculated_cols(df)
+                fcs = sprints_mapping(full_df, fcs_sprints)
+
+                save_data_to_excel(args.output, full_df, fcs)
 
             except Exception as e:
                 l.error("Exception happened boards search " + str(e), e)
@@ -59,3 +60,30 @@ def calculated_cols(df):
     df['Remaining days'] = df['Remaining hours'] / 8
 
     return df
+
+
+def create_pivot(df):
+    pivot = pd.pivot_table(df, index=["Sprint"], values=["PERT Estimation (calculated)"], columns=["Team"],
+                           aggfunc=[np.sum], fill_value=0)
+
+    return pivot
+
+
+def sprints_mapping(df, FC_sprints):
+    FC_reverse = dict((v, k) for k in FC_sprints for v in FC_sprints[k])
+
+    df['Stages'] = df['Sprint'].map(FC_reverse)
+    fcs = df['Stages'].dropna().unique()
+    return fcs
+
+
+def save_data_to_excel(file, data, fcs):
+    with pd.ExcelWriter(file, engine='openpyxl') as writer:
+        writer.book = load_workbook(file)
+        writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
+
+        for fc in fcs:
+            fc_data = data[data['Stages'] == fc]
+            fc_data.to_excel(writer, sheet_name=fc, index=False)
+            pivot_table = create_pivot(fc_data)
+            pivot_table.to_excel(writer, sheet_name=fc + '_Domain_View')

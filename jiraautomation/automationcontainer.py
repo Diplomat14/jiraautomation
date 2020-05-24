d37370e3f85103aa3004eef1aca738762e05c7c4
Sofiya Karpa <sofiya.karpa@apostera.com>
2020-05-24 16:24:42 +0300

    Added automation container

---
+++ jiraautomation/automationcontainer.py
@@ -0,0 +1,28 @@
+from jiraautomation.automationcore import automationcore
+from xdev.core.logger import logger
+
+
+class AutoContainer:
+    __logger = None
+
+    @staticmethod
+    def name():
+        return "AutoContainer"
+
+    def __init__(self, iLogger:logger):
+        assert isinstance(iLogger, logger)
+        self.__logger = iLogger
+
+    @property
+    def logger(self):
+        return self.__logger
+
+    def __setstate__(self, state):
+        self.__logger = state['__logger']
+
+    def init_jira(self, args):
+        op = 'InitJira'
+        op_class = automationcore.get_operation_class(op)
+        op_instance = op_class(self.__logger)
+        container = op_instance.execute(self, args)
+        return container
